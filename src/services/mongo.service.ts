1069e560ce5463bbab0d2912335459e34e1e0b8b
amcoddieng <Dieng0097@gmail.com>
2025-10-03 16:23:30 +0000

    ok

--- src/services/mongo.service.ts
+++ src/services/mongo.service.ts
@@ -1,7 +1,7 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
 
-const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017"
+const uri = process.env.MONGO_URI || "mongodb+srv://dieng0097_db_user:AMeBE2h5Pu1xPDk7@cluster0.w6u76du.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 const client = new MongoClient(uri, {
   serverApi: {
