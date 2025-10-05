39cacad6b0dea419bd6c026061df4cd97257b09b
amcoddieng <Dieng0097@gmail.com>
2025-10-05 20:28:02 +0000

    Update upload.ts

--- src/middlewres/upload.ts
+++ src/middlewres/upload.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, ".././public/uploads"));
+    cb(null, path.join(__dirname, "../../public/uploads"));
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + "-" + file.originalname);
